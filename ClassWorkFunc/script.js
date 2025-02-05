e20448166847bd31c4c3b36418979d5345fe52ea
Ali2010-aa <ismayilzadeli2010@gmail.com>
2025-02-05 11:50:44 +0400

    ev tapsiriqi bitdi

---
+++ ClassWorkFunc/script.js
@@ -0,0 +1,4 @@
+function calculate(currentYear, birthDay) {
+    return (currentYear - birthDay)
+};
+console.log(calculate(2025, 2010));
---
+++ FunctionHomeWork/script.js
@@ -0,0 +1,16 @@
+// let word = prompt("Bir soz daxil edin!")
+// let repeat = +prompt("Tekrarlanma sayi daxil edin!")
+
+// function wordRepeat(a) {
+//     return(a);
+// }
+// for(let i = 0; i < repeat; i++){
+//     console.log(wordRepeat(word));
+// }
+
+
+let word = prompt("Bir soz daxil edin")
+function length(a){
+    return(a);
+}
+console.log(length(word.length));
