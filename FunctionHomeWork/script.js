dd2fc653e9dcd7cfe572465480deaef8238b067e
Ali2010-aa <ismayilzadeli2010@gmail.com>
2025-02-07 19:20:04 +0400

    ev tapsiriqi bitdi

--- FunctionHomeWork/script.js
+++ FunctionHomeWork/script.js
@@ -1,16 +1,16 @@
-// let word = prompt("Bir soz daxil edin!")
-// let repeat = +prompt("Tekrarlanma sayi daxil edin!")
+let word = prompt("Bir soz daxil edin!");
+let repeat = +prompt("Tekrarlanma sayi daxil edin!");
+function wordRepeat(word , repeat) {
+    let result = "";
+    for(let i = 0; i < repeat; i++) {
+        result += word
+    }
+    return result;
+};
+console.log(wordRepeat(word, repeat));
 
-// function wordRepeat(a) {
+// let word = prompt("Bir soz daxil edin")
+// function length(a){
 //     return(a);
 // }
-// for(let i = 0; i < repeat; i++){
-//     console.log(wordRepeat(word));
-// }
-
-
-let word = prompt("Bir soz daxil edin")
-function length(a){
-    return(a);
-}
-console.log(length(word.length));
+// console.log(length(word.length));
