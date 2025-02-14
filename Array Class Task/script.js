58ad8f4d3eb616dcde2b4c3cc7b62902a27f6ddb
Ali2010-aa <ismayilzadeli2010@gmail.com>
2025-02-14 21:30:41 +0400

    tapsiriq bitti

---
+++ Array Class Task/script.js
@@ -0,0 +1,5 @@
+let arr = ["ismayil", "eli", "fazil", "deniz", "jasmin"];
+let sortedArr = arr.toSorted();
+let reversedArr = sortedArr.toReversed();
+console.log(sortedArr);
+console.log(reversedArr);
--- Home Work/script.js
+++ Home Work/script.js
@@ -5,9 +5,17 @@ let num3 = +prompt("3-cu ededi daxil edin!");
 let num4 = +prompt("4-cu ededi daxil edin!");
 let num5 = +prompt("5-ci ededi daxil edin!");
 
-arr.push(num1);
-arr.push(num2);
-arr.push(num3);
-arr.push(num4);
-arr.push(num5);
-console.log(arr);
+arr.push(num1, num2, num3, num4, num5);
+
+let max = arr[0];
+let min = arr[0];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];
+  } else if (arr[i] < min) {
+    min = arr[i];
+  }
+}
+alert(`En boyuk eded:${max}`);
+alert(`En kicik eded:${min}`);
