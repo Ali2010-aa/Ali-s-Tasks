82ab1d6a0cbb81c8c9cf660af5a497ad2b850ee9
Ali2010-aa <ismayilzadeli2010@gmail.com>
2025-02-15 23:33:15 +0400

    tapsiriq bitti

--- ArrayHomeTask/script.js
+++ ArrayHomeTask/script.js
@@ -1,6 +1,6 @@
 let arr = ["Baku", "Ankara", "Paris", "Tehran", "Moskva", "New Yord"];
 let sortedArr = arr.toSorted();
 let sliceArr = sortedArr.slice(0, 3);
-console.log(arr);
-console.log(sortedArr);
+// console.log(arr);
+// console.log(sortedArr);
 console.log(sliceArr);
