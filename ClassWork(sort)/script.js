237c93a398c19ab5235efe77a1d2e2b711871ffe
Ali2010-aa <ismayilzadeli2010@gmail.com>
2025-02-17 21:57:32 +0400

    tapsiriq bitti

--- ClassWork(sort)/script.js
+++ ClassWork(sort)/script.js
@@ -1,10 +1,23 @@
-let arr = ["javascript", "html", "css", "react", "nodejs", "typescript"];
+// let arr = ["javascript", "html", "css", "react", "nodejs", "typescript"];
 
-let sortedArr = arr.sort((a, b) => a.length - b.length);
+// let sortedArr = arr.sort((a, b) => a.length - b.length);
 
-let shortest = sortedArr[0];
+// let shortest = sortedArr[0];
 
-let longest = sortedArr[sortedArr.length - 1];
+// let longest = sortedArr[sortedArr.length - 1];
 
-console.log(shortest);
-console.log(longest);
+// console.log(shortest);
+// console.log(longest);
+
+let evenNums = [];
+let randomNum = Math.floor(Math.random() * 10);
+if (randomNum === 0) {
+  console.log("Random eded sifirdir bir daha yoxla:)");
+} else if (randomNum % 2 === 0) {
+  console.log("Random eded cutdur.");
+  evenNums.push(randomNum);
+  console.log(evenNums);
+} else {
+  console.log("Random eded tekdir.");
+  console.log(randomNum);
+}
