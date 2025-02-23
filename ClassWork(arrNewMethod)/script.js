e75376fa36377b29af07314d732fa58d240b4577
Ali2010-aa <ismayilzadeli2010@gmail.com>
2025-02-23 17:59:20 +0400

    sinif tapsiriqlarinin tekrari

--- ClassWork(arrNewMethod)/script.js
+++ ClassWork(arrNewMethod)/script.js
@@ -1,14 +1,14 @@
 // let prices = [200, 500, 1000, 400, 600];
 // prices.forEach((value) => {
-// console.log(value + value * 0.1)
-// })
+//   console.log(value + value * 0.1);
+// });
 
-// let names = ["ali", "ayse", "mehmet", "zehra"];
+// let names = ["ali", "ayse", "leyla", "zehra"];
 // let result = names.map((value) => {
 //   return value.length;
 // });
 // console.log(result);
 
-let nums = [3, 4, 5, 2, 9, 10, 6];
-let result = nums.filter((value) => value % 2 === 0);
-console.log(result);
+let arr = [13, 4, 5, 2, 9, 10, 6];
+let filteredArr = arr.filter((value) => value % 2 == 0);
+console.log(filteredArr);
