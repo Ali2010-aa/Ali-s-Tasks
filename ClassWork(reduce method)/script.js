8956987b349497c3177c6283b63463980837122d
Ali2010-aa <ismayilzadeli2010@gmail.com>
2025-03-02 14:30:05 +0400

    sinif tapsiriqi

---
+++ ClassWork(reduce method)/script.js
@@ -0,0 +1,14 @@
+// let words = ["alma", "armud", "banan", "çiyələk"];
+
+// let newArr = words.reduce((max, curr) => (curr.length > max.length ? curr : max));
+// console.log(newArr);
+
+// const users = ["Ali" , "Veli" , "Zara" , "Kamran"];
+// function check (){
+//     return users.some((value) => value === "Ali")
+// }
+// console.log(check());
+
+const numbers = [2, 5, 8, 12, 19];
+let result = numbers.every((value) => value > 0);
+console.log(result);
--- HomeWorj(hediyyeli olan)/script.js
+++ HomeWorj(hediyyeli olan)/script.js
@@ -1,4 +1,4 @@
-let numbers = [3, 7, 2, 8, 5];
+let numbers = [3, 7, 7, 8, 5];
 console.log(numbers);
-let result = numbers.map((value) => value + numbers.indexOf(value));
+let result = numbers.map((value ,index) => value += index);
 console.log(result);
