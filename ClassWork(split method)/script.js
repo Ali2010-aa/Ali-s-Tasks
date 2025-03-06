5d157beab8218dc475d30183a26c799c993a81af
Ali2010-aa <ismayilzadeli2010@gmail.com>
2025-03-06 10:15:02 +0400

    sinif tapsiriqi

---
+++ ClassWork(split method)/script.js
@@ -0,0 +1,18 @@
+// const sentence = "frontend development is funny";
+// let sentenceArr = sentence.split(" ");
+// console.log(sentenceArr.reverse().join(" "));
+
+// const sentence = "I love programing";
+// function cevirme() {
+//   let sentenceArr = sentence.split(" ");
+//   return sentenceArr.length;
+// }
+// console.log(cevirme());
+
+const str = "banana mango apple orange";
+let arr = str.split(" ");
+
+let sortedArr = arr.sort();
+
+let sortedStr = sortedArr.join(" ");
+console.log(sortedStr);
