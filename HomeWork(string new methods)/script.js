e8ada6d0074c7d3ef4361095555ac4e6a963eb4d
Ali2010-aa <ismayilzadeli2010@gmail.com>
2025-10-03 21:38:45 +0400

    Delete HomeWork(string new methods) directory

--- HomeWork(string new methods)/script.js
+++
@@ -1,6 +0,0 @@
-function myFunc(text){
-let nameSurname = text.trim();
-let nameSurnameArr = nameSurname.split(" ");
-return nameSurnameArr[nameSurnameArr.length - 1]
-}
-console.log(`Sizin Soyadınız : ${myFunc("      Ali Ismayilzade      ")}`);
