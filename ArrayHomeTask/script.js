f3b06cf01ce60811259b377885f51e73071e8d08
Ali2010-aa <ismayilzadeli2010@gmail.com>
2025-10-03 21:34:00 +0400

    Delete ArrayHomeTask directory

--- ArrayHomeTask/script.js
+++
@@ -1,6 +0,0 @@
-let arr = ["Baku", "Ankara", "Paris", "Tehran", "Moskva", "New York"];
-let sortedArr = arr.toSorted();
-let sliceArr = sortedArr.slice(0, 3);
-// console.log(arr);
-// console.log(sortedArr);
-console.log(sliceArr);
