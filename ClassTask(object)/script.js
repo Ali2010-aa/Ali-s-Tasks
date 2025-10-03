3581ea3ff3cb20e11e435935bb9adb882a25d549
Ali2010-aa <ismayilzadeli2010@gmail.com>
2025-10-03 21:34:20 +0400

    Delete ClassTask(object) directory

--- ClassTask(object)/script.js
+++
@@ -1,9 +0,0 @@
-let object = {
-  name: "Ali",
-  surName: "Ismayilzade",
-  age: 15,
-};
-
-document.querySelector(".name").innerHTML = object.name;
-document.querySelector(".surName").innerHTML = object.surName;
-document.querySelector(".age").innerHTML = object.age;
